763afad8a186213a2cb07548ee37e76deabc1ab5
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-12-16 15:58:34 +0100

    trying to make milestone2

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@
 // Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
 // Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
 
+//Milestone 2
+// Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, 
+// visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+// Click sul contatto mostra la conversazione del contatto cliccato
+
+
 const { createApp } = Vue
 
 createApp({
@@ -179,10 +185,15 @@ createApp({
         }
     ],
     userText: '',
-    
+    activeIndex: 0,
     }
 },
 methods : {
+
+    actualConversation(currentIndex){
+        this.activeIndex === currentIndex;
+    },
+
     addNewMessage(){
         let newMessage = {
             date: '10/01/2020 15:51:00',
